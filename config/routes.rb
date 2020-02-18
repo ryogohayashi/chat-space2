31075defa52c452f87b12e62acb8ffeed80c3dad
ryogohayasi <hamoyashi0923@gmail.com>
2020-02-18 14:31:35 +0900

    create search2

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
-  # :indexを追記
+  root "groups#index"
   resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
-    namespace :api do
-      resources :messages, only: :index, defaults: { format: 'json' }
-    end
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create, ]
   end
 end
